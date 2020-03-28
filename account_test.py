f91aaa4d309fc698af73b46cce0a110e74757274
luiz <luizgfranca@protonmail.com>
2020-03-28 14:47:29 -0300

    Created test for creation of account on database

--- account_test.py
+++ account_test.py
@@ -6,6 +6,16 @@ class AccountTest(unittest.TestCase):
         acc = ac.Account("test")
         self.assertEqual(acc.getName(), "test", "Assert if account is correctly created and test is set")
 
+    def test_write(self):
+        acc = ac.Account("test")
+        acc.setValue(25)
+        acc.setDescription("This is a test account")
+        accId = acc.saveOnDatabase()
+
+        ac2 = ac.getFromDatabase(accId)
+        self.assertEqual(ac2.getName() + "-" + ac2.getDescription(), "test-This is a test account",
+                "Assert if the creation and deletion on database works")            
+
 
 if __name__ ==  '__main__' :
     unittest.main()
