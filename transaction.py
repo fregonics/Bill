b3c7e5d1b1b9ffcb0dc32e88deddb577e20ec9c5
luiz <luizgfranca@protonmail.com>
2020-04-10 00:37:42 -0300

    Created base of transaction class

---
+++ transaction.py
@@ -0,0 +1,50 @@
+import account as acc
+import transaction_db
+
+class Transaction:
+    def __init__(self, data):
+        self.id = None
+        self.origin_acc_id = data[0]
+        self.destiny_acc_id = data[1]
+        self.description = data[2]
+        self.value = data[3]
+    
+    # GET METHODS
+    def getDescription(self):
+        return self.description
+    
+    def getValue(self):
+        return self.value
+
+    def getOrigin(self):
+        return acc.getFromDatabase(self.origin_acc_id)
+    
+    def getDestiny(self):
+        return acc.getFromDatabase(self.destiny_acc_id)
+
+    def getId(self):
+        return self.id
+    
+    def isExecuted(self):
+        return (not self.id == none)
+
+    # SET METHODS
+    def setDescription(self, _description):
+        self.description = _description
+    
+    def setValue(self, _value):
+        self.value = _value
+    
+    def setOrigin(self, _origin):
+        self.origin_acc_id = _origin
+
+    def setDestiny(self, _destiny):
+        self.destiny_acc_id = _destiny
+    
+    def setId(self, _id):
+        self.id = _id
+
+    #PERSISTENT STORAGE
+    def commit(self):
+        self.id = transaction_db.write(self)
+        return self.id
